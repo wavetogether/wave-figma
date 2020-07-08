a9839ccdf6a9bdcc52ab26b68c3181415b38cd1c
Jay Jung <jay@wavetogether.com>
2020-07-08 17:47:57 +0900

    Convert icc files to arr json

---
+++ export-assets/profiles.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const PROFILE_DIR = './profiles';
+
+const names = fs.readdirSync(PROFILE_DIR);
+const profiles = {};
+
+names.forEach(name => {
+    if (!name.endsWith('icc')) {
+        return;
+    }
+
+    const profile = fs.readFileSync(`${PROFILE_DIR}/${name}`);
+    const data = new Uint8Array(profile.length);
+
+    for (let i = 0; i < profile.length; i++) {
+        data[i] = profile[i];
+    }
+
+    profiles[name.slice(0, -4)] = Array.from(data);
+});
+
+fs.writeFileSync('profiles.json', JSON.stringify(profiles));
+console.log('Convert color profiles (.icc) to arrays successfully');
