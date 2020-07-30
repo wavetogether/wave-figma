b1b89e3109e76880fd0dd52ec39448b988cd5d6d
Jay Jung <jay@wavetogether.com>
2020-07-31 00:18:57 +0900

    Add functions for getting selected nodes and exporting svg strings from the nodes

---
+++ convert-icon-to-font/src/utils/fetch.ts
@@ -0,0 +1,28 @@
+export function getSelectedNodes (): ReadonlyArray<SceneNode> {
+    const nodes = figma.currentPage.selection;
+
+    if (nodes.length === 0) {
+        figma.notify('⚠️ There is no selection to export');
+        return [];
+    }
+
+    return nodes;
+}
+
+export async function exportGlyphs (
+    nodes: ReadonlyArray<SceneNode>
+): Promise<string[]> {
+    const glyphs: Array<string> = [];
+    const options: ExportSettingsSVG = { format: 'SVG' };
+
+    for (let node of nodes) {
+        const raw = await node.exportAsync(options);
+        const glyph = String.fromCharCode.apply(null, raw);
+
+        glyphs.push(glyph);
+    }
+
+    return new Promise(resolve => {
+        resolve(glyphs);
+    });
+}
