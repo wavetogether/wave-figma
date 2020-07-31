ae7d66ae6d01aacabac7b9e60cf0849934673202
Jay Jung <jay@wavetogether.com>
2020-07-31 12:09:09 +0900

    Add error on saving and initial logic for saving the font

--- convert-icon-to-font/src/index.ts
+++ convert-icon-to-font/src/index.ts
@@ -1,17 +1,37 @@
-import { getSelectedNodes, exportGlyphs } from './utils/fetch';
+import { getSelectedNodes, exportGlyphs, hasDuplicatedNames } from './utils/fetch';
+import {convertGlyphToData, getFontConfig} from "./utils/convert";
 
 async function convertIconToFont (
-    fontName: string = 'Wave Icon'
+    nodes: ReadonlyArray<SceneNode>,
+    fontName: string = 'WaveIcon'
 ) {
+    const glyphs = await exportGlyphs(nodes);
+    const glyphData = convertGlyphToData(glyphs, nodes);
+    const fontConfig = getFontConfig(glyphData, fontName);
+}
+
+async function onSave (fontName: string = 'WaveIcon') {
     const nodes = getSelectedNodes();
-    const glyphs = exportGlyphs(nodes);
+
+    if (hasDuplicatedNames(nodes)) {
+        figma.ui.postMessage({
+            type: 'err: duplicated-names'
+        });
+
+        figma.notify('⚠️ ERR: There are duplicated names in your selected nodes');
+
+        return;
+    }
+
+    // const [fontBuffer, fontConfig] = await convertIconToFont(nodes, fontName);
 }
 
 async function onMessage ({ type, data }) {
     switch (type) {
-        case 'onRequestSave':
+        case 'req: save':
+            await onSave();
             break;
-        case 'onRequestDone':
+        case 'req: done':
             break;
         default:
             figma.notify('⚠️ ERR: Unknown message type')
