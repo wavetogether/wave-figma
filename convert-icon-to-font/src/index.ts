5e94b5f4a652251266d6f5e45d6c3b567ee35977
Jay Jung <jay@wavetogether.com>
2020-07-31 11:49:44 +0900

    Add initialize sequence

--- convert-icon-to-font/src/index.ts
+++ convert-icon-to-font/src/index.ts
@@ -1,8 +1,26 @@
 import { getSelectedNodes, exportGlyphs } from './utils/fetch';
 
-async function icon2font (
+async function convertIconToFont (
     fontName: string = 'Wave Icon'
 ) {
     const nodes = getSelectedNodes();
     const glyphs = exportGlyphs(nodes);
 }
+
+async function onMessage ({ type, data }) {
+    switch (type) {
+        case 'onRequestSave':
+            break;
+        case 'onRequestDone':
+            break;
+        default:
+            figma.notify('⚠️ ERR: Unknown message type')
+    }
+}
+
+function init () {
+    figma.showUI(__html__);
+    figma.ui.onmessage = onMessage;
+}
+
+init();
